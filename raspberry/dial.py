63fc60ca2dab2e2dd3c9245226ff82712653a001
basismit <basismit@student.artevelde.be>
2017-10-17 14:59:10 +0200

    add python script

---
+++ raspberry/dial.py
@@ -0,0 +1,15 @@
+import flotilla
+import time
+
+dock = flotilla.Client()
+dial = dock.first(flotilla.Dial)
+number = dock.first(flotilla.Number)
+
+try:
+    while True:
+        pos = int(dial.position)
+        print(pos)
+        number = pos
+        time.sleep(1)
+except KeyboardInterrupt:
+    dock.stop()
