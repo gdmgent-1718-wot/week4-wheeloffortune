7affce4589552d30a2ea60af51401e53ed7fb7cc
basismit <basismit@student.artevelde.be>
2017-10-18 10:46:34 +0200

    experiment motor

--- raspberry/dial.py
+++ raspberry/dial.py
@@ -10,18 +10,31 @@ number = dock.first(flotilla.Number)
 motor = dock.first(flotilla.Motor)
 print(motor)
 
+
 cred = credentials.Certificate('wheeloffortune-18d6d-firebase-adminsdk-vn804-4f020fab3d.json')
 default_app = firebase_admin.initialize_app(cred,{'databaseURL':'https://wheeloffortune-18d6d.firebaseio.com/'})
 
 root = db.reference()
+turning = True
+speed = 63
 
 try:
-    while True:
-        #posdb = root.child('value').get()
-        #print(posdb['value'])
-        motor.set_speed(20)
+    while turning:
+        posdb = root.child('value').get()
+        print(posdb['value'])
+        #min value = 6 1 tour 13.70
+        #10 value = tour 7.10
+        #15 value = tour 4.40
+        #30 value = tour 2.20
+        #40 value = tour 1.65
+        motor.set_speed(speed)
         motor.update()
-        time.sleep(5)
+        speed - 1
+        if (speed == 6)
+            motor.stop()
+            turning = False
+        time.sleep(60)
         motor.stop()
+        turning = False
 except KeyboardInterrupt:
     dock.stop()
