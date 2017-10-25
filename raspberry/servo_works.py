885bbfffa83bb4a8a61a3d846e07d341ef077320
adriglib <adriglib@student.arteveldehs.be>
2017-10-25 11:54:44 +0200

    working wheel of fortune

--- raspberry/servo_works.py
+++ raspberry/servo_works.py
@@ -1,32 +1,57 @@
-#!/usr/bin/env python
-#================================================
-#
-#	This program is for SunFounder SuperKit for Rpi.
-#
-#	Extend use of 8 LED with 74HC595.
-#
-#	Change the	WhichLeds and sleeptime value under
-#	loop() function to change LED mode and speed.
-#
-#=================================================
-
+import firebase_admin
+from firebase_admin import  credentials
+from firebase_admin import db
 import RPi.GPIO as GPIO
 import time
 
+
+cred = credentials.Certificate('wheeloffortune-18d6d-firebase-adminsdk-vn804-4f020fab3d.json')
+default_app = firebase_admin.initialize_app(cred,{'databaseURL':'https://wheeloffortune-18d6d.firebaseio.com/'})
+
+root = db.reference()
 GPIO.setmode(GPIO.BOARD)
 GPIO.setup(18, GPIO.OUT)
 
-f = 20
+f = 50
 t = (1 / f)
 
 p = GPIO.PWM(18, f)
 p.start(2.5)
+stopped = False
 
 
+try:
+	while True:
+		posdb = root.child('value').get()
+		picked = posdb['picked']
+		value = posdb['value']
+		print "picked:  " + str(picked) + " value:  " + str(value) + " stopped:  " + str(stopped)
 
-while True:
-	p.ChangeDutyCycle(2.5)
-	time.sleep(t*100)
-	p.ChangeDutyCycle(7.5)
-	time.sleep(t*100)
+		if picked:
+			if stopped == False:
+				print "stopping"
+				stopped = True
+				p.ChangeDutyCycle(value)
+				time.sleep(5)
+				p.stop()
+				GPIO.cleanup()
 
+			else:
+				print "stopped"		
+		else:
+			if stopped: 
+				print "restarting"
+				stopped = False
+				p.start(2.5)
+			print "busy"
+			p.ChangeDutyCycle(2.5)
+			time.sleep(1)
+
+			p.ChangeDutyCycle(12)
+			time.sleep(1)
+			
+		
+
+except KeyboardInterrupt:
+	p.stop()
+	GPIO.cleanup()
