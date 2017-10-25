f958f2854474f8e20071114a48a9d10a243e679b
adriglib <adriglib@student.arteveldehs.be>
2017-10-25 11:56:51 +0200

    working frontend that sends a random piece to firebase

--- frontend/halfpie/js/index.js
+++ frontend/halfpie/js/index.js
@@ -11,8 +11,35 @@ var data = {
         20,
         20,
         20,
-        20]
-};
+        20],
+  moreInfo: [
+      {
+          index: 0,
+          class: '.ct-series-a',
+          dutyCycle: 11
+      },
+      {
+          index: 1,
+          class: '.ct-series-b',
+          dutyCycle: 9
+      },
+      {
+          index: 2,
+          class: '.ct-series-c',
+          dutyCycle: 7.5
+      },
+      {
+          index: 3,
+          class: '.ct-series-d',
+          dutyCycle: 5
+      },
+      {
+          index: 4,
+          class: '.ct-series-e',
+          dutyCycle: 3.5
+      },
+  ]};
+
 
 var settings = {
   total: 200, // 2x total for half-pie chart
