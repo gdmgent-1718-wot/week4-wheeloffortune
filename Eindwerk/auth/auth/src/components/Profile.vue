2be2ee44e4afa0de52dc161741180f338dcd59ec
adriglib <adriglib@student.arteveldehs.be>
2017-12-05 13:02:14 +0100

    game

--- Eindwerk/auth/auth/src/components/Profile.vue
+++ Eindwerk/auth/auth/src/components/Profile.vue
@@ -2,7 +2,6 @@
     <div class="container">
         <h1>{{ msg }}</h1>
         <button v-on:click="logout">logout</button>
-
     </div>
 </template>
 
@@ -69,19 +68,19 @@
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-h1, h2 {
-  font-weight: normal;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
-</style>
+<!--<style scoped>-->
+<!--h1, h2 {-->
+  <!--font-weight: normal;-->
+<!--}-->
+<!--ul {-->
+  <!--list-style-type: none;-->
+  <!--padding: 0;-->
+<!--}-->
+<!--li {-->
+  <!--display: inline-block;-->
+  <!--margin: 0 10px;-->
+<!--}-->
+<!--a {-->
+  <!--color: #42b983;-->
+<!--}-->
+<!--</style>-->
