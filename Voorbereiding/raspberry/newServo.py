e3ca07f70f614ed158b06286e5b5c101a635f3aa
gdm-1718-basismit <basiel.smitz@student.arteveldehs.be>
2017-12-19 15:15:13 +0100

    motor update

--- Voorbereiding/raspberry/newServo.py
+++ Voorbereiding/raspberry/newServo.py
@@ -1,17 +1,26 @@
-import RPi.GPIO as GPIO
+# Servo Control
 import time
-GPIO.setmode(GPIO.BCM)
-GPIO.setup(7, GPIO.OUT)
-p = GPIO.PWM(7, 50)
-p.start(7.5)
-try:
-        while True:
-                p.ChangeDutyCycle(7.5)
-                time.sleep(1)
-                p.ChangeDutyCycle(12.5)
-                time.sleep(1)
-                p.ChangeDutyCycle(2.5)
-                time.sleep(1)
-
-except KeyboardInterrupt:
-        GPIO.cleanup()
+import wiringpi
+ 
+# use 'GPIO naming'
+wiringpi.wiringPiSetupGpio()
+ 
+# set #18 to be a PWM output
+wiringpi.pinMode(18, wiringpi.GPIO.PWM_OUTPUT)
+ 
+# set the PWM mode to milliseconds stype
+wiringpi.pwmSetMode(wiringpi.GPIO.PWM_MODE_MS)
+ 
+# divide down clock
+wiringpi.pwmSetClock(192)
+wiringpi.pwmSetRange(2000)
+ 
+delay_period = 0.01
+ 
+while True:
+        for pulse in range(50, 250, 1):
+                wiringpi.pwmWrite(18, pulse)
+                time.sleep(delay_period)
+        for pulse in range(250, 50, -1):
+                wiringpi.pwmWrite(18, pulse)
+                time.sleep(delay_period)
