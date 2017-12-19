d2cf649577139c68ff1c65cc328a292100fb65bd
gdm-1718-basismit <basiel.smitz@student.arteveldehs.be>
2017-12-19 14:42:17 +0100

    motor update

--- Voorbereiding/raspberry/servo_works.py
+++ Voorbereiding/raspberry/servo_works.py
@@ -18,18 +18,18 @@ Resources:
     - https://i.stack.imgur.com/sVvsB.jpg
 ==============================================
 """
-# import firebase_admin
-# from firebase_admin import  credentials
-# from firebase_admin import db
-# from firebase_admin import storage
+import firebase_admin
+from firebase_admin import  credentials
+from firebase_admin import db
+from firebase_admin import storage
 import RPi.GPIO as GPIO
 import time
 
 
-# cred = credentials.Certificate('wheeloffortune-18d6d-firebase-adminsdk-vn804-4f020fab3d.json')
-# default_app = firebase_admin.initialize_app(cred,{'databaseURL':'https://wheeloffortune-18d6d.firebaseio.com/','storageBucket': 'gs://wheeloffortune-18d6d.appspot.com'})
-# bucket = storage.bucket()
-# root = db.reference()
+cred = credentials.Certificate('wheeloffortune-18d6d-firebase-adminsdk-vn804-4f020fab3d.json')
+default_app = firebase_admin.initialize_app(cred,{'databaseURL':'https://wheeloffortune-18d6d.firebaseio.com/','storageBucket': 'gs://wheeloffortune-18d6d.appspot.com'})
+bucket = storage.bucket()
+root = db.reference()
 GPIO.setmode(GPIO.BOARD)
 #Connect the yellow cable to GPIO pin 18
 GPIO.setup(18, GPIO.OUT)
@@ -41,26 +41,25 @@ t = (1 / f)
 
 p = GPIO.PWM(18, f)
 p.start(2.5)
-pictureTaken = False
+pictureTaken = false
 try:
 	while True:
 		#get values from Firebase and put them in variables
-		# posdb = root.child('value').get()
-		# picked = posdb['picked']
-		picked = False
+		posdb = root.child('value').get()
+		picked = posdb['picked']
 		# value should be between 2.5 and 12.5
 		# correct values are 11, 9, 7.5, 5, 3.5
-		value = 12
+		value = posdb['value']
 
 		#if result is picked move to result position and stop spinning
 		if picked:
 			print "stopping"
 			p.ChangeDutyCycle(value)
 			time.sleep(1)
-			# if !pictureTaken
-			# 	#take picture
-			# 	pictureTaken = True
-            #
+			if !pictureTaken
+				#take picture
+				pictureTaken = True
+
 		else:
 			
 			print "spinning"
