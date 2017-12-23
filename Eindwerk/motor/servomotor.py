d528e6b7db1b278698649f863b2a4b5f0f0f97e3
gdm-1718-basismit <basiel.smitz@student.arteveldehs.be>
2017-12-23 15:34:39 +0100

    motor

--- Eindwerk/motor/servomotor.py
+++ Eindwerk/motor/servomotor.py
@@ -1,23 +1,12 @@
-import RPi.GPIO as GPIO
-import time
-GPIO.setmode(GPIO.BOARD)
-GPIO.setup(7, GPIO.OUT)
+import machine
+pin = machine.pin(4)
+servo = machine.PWM(pin, freq=50)
 
-f = 50
-t = (1 / f)
+servo.duty(40)
+servo.duty(115)
+servo.duty(77)
 
-p = GPIO.PWM(7, f)
-p.start(2.5)
 
-try:
-    while True:
-        print "motor going 7.5"
-        p.ChangeDutyCycle (12.5)
-        time.sleep(0.5)
-        print 'motor going 2.5'
-        p.ChangeDutyCycle (2.5)
-        time.sleep (0.5)
 
-except KeyboardInterrupt:
-	p.stop()
-	GPIO.cleanup()
+
+
