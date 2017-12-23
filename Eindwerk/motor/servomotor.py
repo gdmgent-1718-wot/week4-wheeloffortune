e80930a17d39168a2f36c7b3713e23ec76bba224
gdm-1718-basismit <basiel.smitz@student.arteveldehs.be>
2017-12-23 15:03:18 +0100

    motor

---
+++ Eindwerk/motor/servomotor.py
@@ -0,0 +1,16 @@
+GPIO.setmode(GPIO.BOARD)
+GPIO.setup(18, GPIO.OUT)
+
+f = 50
+t = (1 / f)
+
+p = GPIO.PWM(18, f)
+p.start(2.5)
+
+try:
+    while True:
+        print "motor turning"
+
+except KeyboardInterrupt:
+	p.stop()
+	GPIO.cleanup()
