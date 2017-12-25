3411a39cad27ab74e10cd298080f1b5cff81e017
gdm-1718-basismit <basiel.smitz@student.arteveldehs.be>
2017-12-26 00:36:51 +0100

    motor

--- Eindwerk/motor/simpleDc.py
+++ Eindwerk/motor/simpleDc.py
@@ -25,19 +25,20 @@ try:
     while True:
         # get values from Firebase and put them in variables
         posdb = root.child('game').child('motor').get()
+        print posdb
         # is motor turning?
-        turning = posdb['turning']
+        #turning = posdb['turning']
         # speed > 70 && < 100
-        speed = posdb['speed']
-        if turning:
-            print 'turning'
-            sleep(2)
+        # speed = posdb['speed']
+        # if turning:
+            # print 'turning'
+            # sleep(2)
             # print("turning")
             # GPIO.output(07, True)
             # pwm.ChangeDutyCycle(speed)
-        else:
-            print 'stopped'
-            sleep(2)
+        # else:
+            # print 'stopped'
+            # sleep(2)
             # print("waiting")
             # GPIO.output(07, False)
 except KeyboardInterrupt:
