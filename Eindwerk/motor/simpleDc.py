b081beadad1c26a771dbf0f26d76b4fc4a7fda2f
gdm-1718-basismit <basiel.smitz@student.arteveldehs.be>
2017-12-25 22:34:26 +0100

    motor

--- Eindwerk/motor/simpleDc.py
+++ Eindwerk/motor/simpleDc.py
@@ -1,6 +1,12 @@
 import RPi.GPIO as GPIO
+import firebase_admin
+from firebase_admin import  credentials
+from firebase_admin import db
 from time import sleep
 
+cred = credentials.Certificate('wheeloffortune-18d6d-firebase-adminsdk-vn804-4f020fab3d.json')
+default_app = firebase_admin.initialize_app(cred,{'databaseURL':'https://wheeloffortune-18d6d.firebaseio.com/'})
+root = db.reference()
 GPIO.setmode(GPIO.BOARD)
 
 GPIO.setup(03, GPIO.OUT)
@@ -13,13 +19,27 @@ pwm.start(0)
 GPIO.output(03, True)
 GPIO.output(05, False)
 
-GPIO.output(07, True)
-pwm.ChangeDutyCycle(100)
-sleep(10)
-pwm.ChangeDutyCycle(50)
-sleep(1)
-pwm.ChangeDutyCycle(10)
-sleep(1)
+try:
+	while True:
+		#get values from Firebase and put them in variables
+		posdb = root.child('game/motor').get()
+        # is motor turning?
+		turning = posdb['turning']
+		# speed > 70 && < 100
+		speed = posdb['speed']
 
-GPIO.output(07, False)
+		if turning:
+            print "turning"
+            GPIO.output(07, True)
+            pwm.ChangeDutyCycle(speed)
+		else:
+            print "waiting"
+			GPIO.output(07, False)
+			
+			
+		
+
+except KeyboardInterrupt:
+	p.stop()
+	GPIO.cleanup()
 
